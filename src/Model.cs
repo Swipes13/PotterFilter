2090c7ceeb825ee8582d3039b1c9bc066b468a16
Valentin <Ualentin92@gmail.com>
2015-11-21 02:27:35 +0400

    data generate. questionExist.

--- src/Model.cs
+++ src/Model.cs
@@ -8,34 +8,53 @@ using PotterFilter.src.math;
 namespace PotterFilter.src {
   class Model {
     const int countObs = 25;
+    Matrix2 _H = new Matrix2(2, new double[] { 1.0, 0.0 });
+    Matrix2 _Г = new Matrix2(2, new double[] {
+      0.0008682425818, 0.03790917931,
+      -0.9477294827, 0.2283233184   
+    });
+    Matrix2 _Ksi = new Matrix2(1, new double[] { 0.02170606454, -23.69323707 });
+    Matrix2 _Phi = new Matrix2(2, new double[] {
+      0.01232548830, 0.0008682425818,
+      -0.02170606454, 0.05227051732  });
 
-    Matrix2D _Rk, _Qk, _P0, _Bk;
-    Matrix2D nu = new Matrix2D(1, 2);
-    Matrix2D omega = new Matrix2D(1, 2);
-    Matrix2D x0 = new Matrix2D(1, 2);
+    Matrix2 _Rk, _Qk, _P0, _Bk;
 
-    Matrix2D _X = new Matrix2D(countObs, 2);
-    Matrix2D _V_Noize = new Matrix2D(1, countObs);
-    Matrix2D _Y_Observe = new Matrix2D(1, countObs);
-    Matrix2D _U_Control = new Matrix2D(1, countObs);
+    Matrix2 _X = new Matrix2(countObs, 2);
+    Matrix2 _V_Noize = new Matrix2(countObs, 1);
+    Matrix2 _Y_Observe = new Matrix2(countObs, 1);
+    Matrix2 _U_Control = new Matrix2(countObs, 1);
+    Matrix2 _W_Disturb = new Matrix2(countObs, /*???? 1 ???*/ 2);
 
-    double[] nullExpected = new double[]{ 0.0, 0.0};
+    double[] amplitudeA = new double[] { 0.0 };
+    double[] nullExpected = new double[] { 0.0, 0.0 };
     double[] x0Expected = new double[] { 0.0, 0.0 };
 
     public Model() {
       double rkD = 0.001, qkD = 0.001, p0D = 0.001, bkD = 0.001;
-      _Rk = Matrix2D.Diagonal(new double[] { rkD, rkD });
-      _Qk = Matrix2D.Diagonal(new double[] { qkD, qkD });
-      _P0 = Matrix2D.Diagonal(new double[] { p0D, p0D });
-      _Bk = Matrix2D.Diagonal(new double[] { bkD, bkD });
+      _Rk = Matrix2.Diagonal(new double[] { rkD });
+      _Qk = Matrix2.Diagonal(new double[] { qkD, qkD });
+      _P0 = Matrix2.Diagonal(new double[] { p0D, p0D });
+      _Bk = Matrix2.Diagonal(new double[] { bkD });
 
       GenerateData();
     }
     bool GenerateData() {
-      if (Matrix2D.MultiDistr(nullExpected, _Rk, ref nu) == false) return false;
-      if (Matrix2D.MultiDistr(nullExpected, _Qk, ref omega) == false) return false;
-      if (Matrix2D.MultiDistr(x0Expected, _P0, ref x0) == false) return false;
+      Matrix2 x0 = new Matrix2(1, 2);
+      if (Matrix2.MultiDistr(amplitudeA, _Bk, ref _U_Control) == false) return false;
+      if (Matrix2.MultiDistr(x0Expected, _P0, ref x0) == false) return false;
 
+      if (Matrix2.MultiDistr(nullExpected, _Rk, ref _V_Noize) == false) return false;
+      if (Matrix2.MultiDistr(nullExpected, _Qk, ref _W_Disturb) == false) return false;
+
+      _X[0] = x0;
+      _Y_Observe[0] = _H * _X[0].Transpose() +_V_Noize[0];
+
+      for (int i = 1; i < countObs; i++) {
+        _X[i] = (_Phi * _X[i - 1].Transpose() + _Ksi * _U_Control[i] + _Г * _W_Disturb[i].Transpose()).Transpose();
+        _Y_Observe[i] = _H * _X[i].Transpose() + _V_Noize[i];
+      }
+        
       return true;
     }
 
--- src/math/Matrix.cs
+++ src/math/Matrix.cs
@@ -5,8 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PotterFilter.src.math {
-  public class Matrix2D {
+  public class Matrix2 {
     private double[,] matrix;
+    public Matrix2 this[int index] {
+      get {
+        Matrix2 retVal = new Matrix2(1, LenghtY());
+
+        for (int i = 0; i < LenghtY(); i++ )
+          retVal[0, i] = matrix[index, i];
+
+        return retVal;
+      }
+      set {
+        for (int i = 0; i < value.LenghtY(); i++)
+          matrix[index, i] = value[0, i];
+      }
+    }
     public double this[int i,int j] {
       get { return matrix[i, j]; }
       set { matrix[i, j] = value; }
@@ -14,13 +28,13 @@ namespace PotterFilter.src.math {
     public int LenghtX() { return matrix.GetLength(0); }
     public int LenghtY() { return matrix.GetLength(1); }
 
-    public Matrix2D(int size) {
+    public Matrix2(int size) {
       matrix = new double[size, size];
     }
-    public Matrix2D(int sizeX, int sizeY) {
+    public Matrix2(int sizeX, int sizeY) {
       matrix = new double[sizeX, sizeY];
     }
-    public Matrix2D(int sizeX, double[] values) {
+    public Matrix2(int sizeX, double[] values) {
       matrix = new double[values.Count() / sizeX, sizeX];
       int i = 0, j = 0;
       foreach(double val in values){
@@ -42,23 +56,38 @@ namespace PotterFilter.src.math {
           return -1.0;
       }
     }
-    
-    public static Matrix2D Diagonal(double[] dValues) {
-      Matrix2D matrix = new Matrix2D(dValues.Count());
+
+    public static Matrix2 Identity(int dim) {
+      Matrix2 m = new Matrix2(dim, dim);
+      for (int i = 0; i < dim; i++)
+        m[i,i] = 1.0;
+      return m;
+    }
+    public Matrix2 Transpose() {
+      Matrix2 transpose = new Matrix2(LenghtY(), LenghtX());
+
+      for (int i = 0; i < LenghtX(); i++) 
+        for (int j = 0; j < LenghtY(); j++) 
+          transpose[j,i] = this.matrix[i,j];
+      
+      return transpose;
+    }
+    public static Matrix2 Diagonal(double[] dValues) {
+      Matrix2 matrix = new Matrix2(dValues.Count());
       for (int i = 0; i < dValues.Count(); i++) 
         matrix[i, i] = dValues[i];
 
       return matrix;
     }
-    public static bool MultiDistr(double[] expctedValues, Matrix2D correlation, ref Matrix2D result) {
+    public static bool MultiDistr(double[] expctedValues, Matrix2 correlation, ref Matrix2 result) {
       Random rand = new Random();
 
       int count = result.LenghtX();
       int mq = result.LenghtY();
 
-      Matrix2D A = new Matrix2D(mq, mq);
-      Matrix2D N = new Matrix2D(count, mq);
-      Matrix2D K = correlation;
+      Matrix2 A = new Matrix2(mq, mq);
+      Matrix2 N = new Matrix2(count, mq);
+      Matrix2 K = correlation;
 
       if (K.Determinant() <= 0.0) return false;
 
@@ -91,16 +120,15 @@ namespace PotterFilter.src.math {
       }
       return true;
     }
-
-    public static bool MultiDistr(double[] expctedValues, double[] dispersion, Matrix2D correlation, ref Matrix2D result) {
+    public static bool MultiDistr(double[] expctedValues, double[] dispersion, Matrix2 correlation, ref Matrix2 result) {
       Random rand = new Random();
 
       int count = result.LenghtX();
       int mq = result.LenghtY();
 
-      Matrix2D A = new Matrix2D(mq, mq);
-      Matrix2D N = new Matrix2D(count, mq);
-      Matrix2D K = new Matrix2D(mq, mq);
+      Matrix2 A = new Matrix2(mq, mq);
+      Matrix2 N = new Matrix2(count, mq);
+      Matrix2 K = new Matrix2(mq, mq);
 
       //Преобразование корреляционной матрицы в ковариационную
       for (int i = 0; i < mq; i++)
@@ -137,6 +165,26 @@ namespace PotterFilter.src.math {
         }
       }
       return true;
+    }
+
+    static public Matrix2 operator *(Matrix2 m1, Matrix2 m2) {
+      Matrix2 retValue = new Matrix2(m1.LenghtX(), m2.LenghtY());
+
+      for (int i = 0; i < m1.LenghtX(); i++) 
+        for (int j = 0; j < m2.LenghtY(); j++) 
+          for (int k = 0; k < m1.LenghtY(); k++) 
+            retValue[i,j] += m1[i,k] * m2[k,j];
+      
+      return retValue;
+    }
+    static public Matrix2 operator +(Matrix2 m1, Matrix2 m2) {
+      Matrix2 retValue = new Matrix2(m1.LenghtX(), m1.LenghtY());
+
+      for (int i = 0; i < m1.LenghtX(); i++)
+        for (int j = 0; j < m1.LenghtY(); j++)
+          retValue[i, j] = m1[i, j] * m2[i, j];
+
+      return retValue;
     }
   }
 }
